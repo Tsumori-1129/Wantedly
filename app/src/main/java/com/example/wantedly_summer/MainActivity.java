03e9b99c686fa14348f54a5765031eeb9249f47e
tsumo <mt98is12@gmail.com>
2020-07-27 23:42:56 +0900

    ヘッダー、フッターの統合

--- app/src/main/java/com/example/wantedly_summer/MainActivity.java
+++ app/src/main/java/com/example/wantedly_summer/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.wantedly_summer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout mcontent;
+    private ScrollView mscroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
